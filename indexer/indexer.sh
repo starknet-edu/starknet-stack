63b8ce075f23e760b9b4d9e9a394e4ad4837ef64
drspacemn <dontpanicdao@gmail.com>
2023-01-20 11:21:36 -0700

    update indexer syntax

--- indexer/indexer.sh
+++ indexer/indexer.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-CONTRACT_ADDR="0x00806778f9b06746fffd6ca567e0cfea9b3515432d9ba39928201d18c8dc9fdf"
+CONTRACT_ADDR="0x806778f9b06746fffd6ca567e0cfea9b3515432d9ba39928201d18c8dc9fdf"
 
 while true
 do
+    BLOCK_NUM=$(curl -s --location --request POST 'http://localhost:9545' \
+    --header 'Content-Type: application/json' \
+    --data-raw '{
+        "jsonrpc":"2.0",
+        "method":"starknet_getBlockWithTxs",
+        "params":["latest"],
+        "id":1
+    }' | jq -r '.result.block_number')
+    echo "Pulled Block #: $BLOCK_NUM"
+
     TX_HASH=$(curl -s --location --request POST 'http://localhost:9545' \
     --header 'Content-Type: application/json' \
     --data-raw '{
@@ -11,25 +21,25 @@ do
         "method":"starknet_getBlockWithTxs",
         "params":["latest"],
         "id":1
-    }' | jq -r '.result.transactions[] | .contract_address + " " + .transaction_hash' | grep $CONTRACT_ADDR | awk '{print $2}')
+    }' | jq -r '.result.transactions[] | .calldata[] + " " + .transaction_hash' 2>/dev/null | grep $CONTRACT_ADDR | awk '{print $2}')
 
     if [ -z "$TX_HASH" ]; then
-        echo "no transaction for contract"
+        echo "no transaction for contract\n"
     else
         echo "found transaction: $TX_HASH"
 
-        echo "Eevents to Index:"
+        echo "Events to Index:"
 
-        curl --location --request POST 'http://localhost:9545' \
+        curl -s --location --request POST 'http://localhost:9545' \
             --header 'Content-Type: application/json' \
             --data-raw '{
                 "jsonrpc":"2.0",
                 "method":"starknet_getTransactionReceipt",
-                "params":["0x05dc7fd94d54b4b1bf4229fe3b306164d5bab5cf2f4ac59b1fd8bbd5605244c5"],
+                "params":["'$TX_HASH'"],
                 "id":1
-            }' | jq -r '.result.events[]'
+            }' | jq -r '.result.events'
         
         exit 0
     fi
-    sleep 60
+    sleep 15
 done
